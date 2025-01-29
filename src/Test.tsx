638670b49b4ba96f03b70f62589ad357085aba70
Cheska Karla Anne C. Noynay <cheskanoynay@gmail.com>
2025-01-30 01:32:20 +0800

    added nativewind

---
+++ src/Test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Test = () => {
+  return (
+    <View>
+      <Text className="text-green-600 text-4xl">hello world</Text>
+    </View>
+  );
+};
+
+export default Test;
